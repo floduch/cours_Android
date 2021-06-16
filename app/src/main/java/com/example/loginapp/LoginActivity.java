d729915c6631160305db66335046667eb4131f68
floduch <florian.duchaine29@gmail.com>
2021-06-16 10:18:10 +0200

    first commit - page de connexion

---
+++ app/src/main/java/com/example/loginapp/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText
+    }
+}
