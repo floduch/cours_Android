634d593d15f5883bf335cd3bb553681eeac7aba3
floduch <florian.duchaine29@gmail.com>
2021-06-16 10:38:27 +0200

    page de connexion avec renvoie sur l'activité suivante

--- app/src/main/java/com/example/loginapp/LoginActivity.java
+++ app/src/main/java/com/example/loginapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,11 +21,17 @@ public class LoginActivity extends AppCompatActivity {
 
         Button connect = findViewById(R.id.buttonConnect);
 
+        Intent mainActivity = new Intent(this, MainActivity.class);
+
         connect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 Toast myToast = Toast.makeText(LoginActivity.this,  "Bonjour "+ identifiant.getText().toString(), Toast.LENGTH_SHORT);
                 myToast.show();
+
+                mainActivity.putExtra("id", identifiant.getText().toString());
+                mainActivity.putExtra("pass", password.getText().toString());
+                startActivity(mainActivity);
 
             }
         });
---
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.loginapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_round_arrow_back_24);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Bundle extras = getIntent().getExtras();
+        String id, pass;
+        if (extras != null) {
+            id = extras.getString("id");
+            pass = extras.getString("pass");
+            // and get whatever type user account id is
+        }else {
+            id ="0";
+            pass="0";
+        }
+
+        TextView text = findViewById(R.id.text);
+
+        text.setText("Bienvenue "+ id + ", votre mot de passe est "+pass);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
